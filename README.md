37b7f6fe7fbcabf147852b4cc9a60076455443d6
amazimpaka <aloys.mazimpaka@uxpsystems.com>
2018-08-08 21:30:35 -0400

    initial Spring boot project

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # microlab
+
+A simple Spring boot application created from: https://start.spring.io/ with the 4 following dependencies:
+- Web
+- DevTools
+- Starter JPA
+- H2
+
 Microservices Lab
---
+++ src/main/java/io/lab/microservice/MicroLabApplication.java
@@ -0,0 +1,12 @@
+package io.lab.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroLabApplication.class, args);
+	}
+}
